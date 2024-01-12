ae06e6696387eccad6db5f9896fc5030c5d1895e
Jerryhqrrr <jhu@webb.org>
2024-01-11 17:32:03 -0800

    jonathan cookage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOP.java
@@ -17,11 +17,18 @@ import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
 @TeleOp(name="TeleOP", group="TeleOP")
 public class TeleOP extends LinearOpMode {
 
-    double claw1Grab = 0.667;
-    double claw2Grab = 0.667;
+    //positions to tune -------------------------
+    double claw1Grab = 0.51;
+    double claw2Grab = 0.28;
+    double wrist1Pos = 0.2;
+    double wrist2Pos = 0.4;
+    double arm1Pos = 0.3;
+    double arm2Pos = 0.7;
+    //-------------------------------------------
+
+    //other stuff idk
     Gamepad currentGamepad1, previousGamepad1;
     Gamepad currentGamepad2, previousGamepad2;
-
 
 
     @Override
@@ -31,9 +38,13 @@ public class TeleOP extends LinearOpMode {
 
         waitForStart();
         double straight, turn, strafe;
+
         double liftPower, claw1Pos, claw2Pos, wristPos, armPos;
         int transferState = 0;
-        int dropState = 0;
+        int intakeState = 0;
+
+        //transfer bools
+        boolean intake1 = false;
 
         while (opModeIsActive()) {
             currentGamepad1 = gamepad1;
@@ -50,42 +61,78 @@ public class TeleOP extends LinearOpMode {
             telemetry.addData("y", drive.pose.position.y);
             telemetry.addData("heading (deg)", Math.toDegrees(drive.pose.heading.toDouble()));
 
-            //IMU Feedback data
+            //IMU Feedback data------------------------------------------------------------
             YawPitchRollAngles angles = drive.imu.getRobotYawPitchRollAngles();
             telemetry.addData("Pitch",angles.getPitch(AngleUnit.DEGREES));
             telemetry.addData("Roll",angles.getRoll(AngleUnit.DEGREES));
             telemetry.addData("Yaw",angles.getYaw(AngleUnit.DEGREES));
+            //------------------------------------------------------------------------------
 
 
-            if (currentGamepad1.cross && !previousGamepad1.cross){
-                transferState = (transferState +1)%4;
+            //INTAKE LMAOOOOOOO ------------------------------------------
+            if (gamepad1.right_bumper && intakeState == 0){
+                claw1Pos = claw1Grab;
+                intakeState = 1;
             }
-            if ((currentGamepad1.circle && !previousGamepad1.circle) && transferState >=2){
-                transferState = 4;
+            else if (gamepad1.right_bumper && intakeState == 1){
+                claw2Pos = claw2Grab;
+                intakeState = 2;
             }
+            else if (gamepad1.right_bumper && intakeState == 2){
+                claw2Pos = 0;
+                intakeState = 3;
+            }
+            else if (gamepad1.right_bumper && intakeState == 3){
+                claw1Pos = 0;
+                armPos = 0;
+                wristPos = 0;
+                intakeState = 0;
+                transferState = 0;
+            }
+            //------------------------------------------------------------
 
-//            switch (transferState){
-//                case 0:
-//                    claw1Pos = 0;
-//                    claw2Pos = 0;
-//                    wristPos = 0;
-//                    armPos = 0;
-//                    break;
-//                case 1://grabs two
-//                    claw1Pos = 0.51;
-//                    claw2Pos = 0.28;
-//                    break;
-//                case 2://place front
-//                    wristPos = 0.2;
-//                    armPos = 0.3;
-//                    break;
-//                case 3://place 3
-//                    wristPos = 1.0;
-//                    aPos = 0.7;
-//                    break;
-//
-//                case 6:
-//            }
+            //TRANSFER TSTUFFFFFFFFF ------------------------------------------
+            if (gamepad1.x && transferState == 0){
+                //drop front
+                armPos = arm1Pos;
+                wristPos = wrist1Pos;
+                transferState = 1;
+            }
+            else if (gamepad1.x && transferState == 1){
+                //drop back
+                armPos = arm2Pos;
+                wristPos = wrist2Pos;
+                transferState = 2;
+            }
+            else if (gamepad1.x && transferState == 2){
+                //reset
+                armPos = 0;
+                wristPos = 0;
+                transferState = 0;
+            }
+            //------------------------------------------------------------------
+
+
+            //LIFT LIFT ------------------------------------------------------------
+            else if (gamepad1.left_trigger >= 0.1){
+                //lift go up
+                drive.lift1.setPower(gamepad1.left_trigger);
+                drive.lift2.setPower(-gamepad1.left_trigger);
+            }
+            else if (gamepad1.left_bumper){
+                //lift go down
+                drive.lift1.setPower(-0.4);
+                drive.lift2.setPower(0.4);
+            }
+            else{
+                //lift hold
+                drive.lift1.setPower(0.05);
+                drive.lift2.setPower(-0.05);
+            }
+            //------------------------------------------------------------------
+
+
+            //=---------------------------------------------
             claw1Pos = gamepad2.left_stick_x;
             claw2Pos = gamepad2.right_stick_x;
             //0.28
@@ -95,26 +142,13 @@ public class TeleOP extends LinearOpMode {
             //0.7 all the way back, 0.05 off the ground a bit, assuming 0.3 for front placement.
             wristPos = currentGamepad1.right_trigger;
             //1.0 max
+            //------------------------------------------------
 
-            if (gamepad1.)
 
-//            if (gamepad1.a) {
-//
-//            }
-//            else if (gamepad1.b){
-//                armPos = 0;
-//                wristPos = 0;
-//            }else {
-//                armPos = 0.7;
-//                wristPos = 1;
-//            }
             telemetry.addData("armPos",armPos);
             telemetry.addData("wristPos",wristPos);
             telemetry.addData("claw1Pos",claw1Pos);
             telemetry.addData("claw2Pos",claw2Pos);
-
-
-
 
             drive.claw1.setPosition(claw1Pos);
             drive.claw2.setPosition(claw2Pos);
@@ -123,6 +157,5 @@ public class TeleOP extends LinearOpMode {
             drive.arm2.setPosition(armPos);
             telemetry.update();
         }
-
     }
 }
